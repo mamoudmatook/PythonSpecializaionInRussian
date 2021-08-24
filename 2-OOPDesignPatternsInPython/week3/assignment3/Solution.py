1f2afb5d6220fdc78f732c4b174824d37d03d1c5
Maatuq <maatuq.mhamoud@hcl.com>
2021-08-24 12:07:42 +0400

    (course 2/week3) adding solution to assignment 2, 3

---
+++ 2-OOPDesignPatternsInPython/week3/assignment3/Solution.py
@@ -0,0 +1,60 @@
+#
+# Created on Mon Aug 23 2021
+#
+# The MIT License (MIT)
+# Copyright (c) 2021 Maatuq
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+# and associated documentation files (the "Software"), to deal in the Software without restriction,
+# including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all copies or substantial
+# portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+# TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+from abc import ABC, abstractmethod
+
+
+class ObservableEngine(Engine):
+    def __init__(self):
+        self.__subscribers = set()
+
+    def subscribe(self, subscriber):
+        self.__subscribers.add(subscriber)
+
+    def unsubscribe(self, subscriber):
+        self.__subscribers.remove(subscriber)
+
+    def notify(self, message):
+        for subscriber in self.__subscribers:
+            subscriber.update(message)
+
+
+class AbstractObserver(ABC):
+    @abstractmethod
+    def update(self, message):  # Абстрактный наблюдатель задает метод update
+        pass
+
+
+class ShortNotificationPrinter(AbstractObserver):
+    def __init__(self):
+        self.achievements = set()
+
+    def update(self, message):  # Конкретная реализация метода update
+        self.achievements.add(message["title"])
+
+
+class FullNotificationPrinter(AbstractObserver):
+    def __init__(self):
+        self.achievements = list()
+
+    def update(self, message):
+        if message not in self.achievements:
+            self.achievements.append(message)
+        
